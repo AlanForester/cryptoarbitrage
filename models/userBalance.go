7908643e5aae51e15535b3b37e24ef3f2ee02285
Alex Collin <alex@collin.su>
2017-11-15 14:21:48 +0300

    Edit models

--- models/userBalance.go
+++ models/userBalance.go
@@ -8,10 +8,10 @@ import (
 )
 
 type UserBalance struct {
-	kallax.Model         `table:"users" pk:"id,autoincr"`
+	kallax.Model         `table:"user_balances" pk:"id,autoincr"`
 	ID       kallax.ULID `pk:"autoincr"`
 	User     *User       `fk:"user_id,inverse"`
 	Exchange *Exchange   `fk:"exchange_id,inverse"`
-	Asset    string
-	Volume   float32
+	Asset    *Asset      `fk:"asset_id,inverse"`
+	Volume   float32     `kallax:"volume"`
 }
