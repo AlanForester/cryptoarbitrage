ad6d3168b65d267a7df5f05960cd631d93c638e5
Alex Collin <alex@collin.su>
2017-11-15 21:55:31 +0300

    Add migrations

--- common.mk
+++ common.mk
@@ -9,16 +9,26 @@ stop:
 	go run main.go -d stop
 
 env:
-		pwd := $$(pwd)
-		export GOBIN=$(pwd)/bin
-		export GOPATH=$(pwd):$$(pwd)/../..:~/go
-		export PATH=$(PATH):$$(pwd)/bin
+	pwd := $$(pwd)
+	export GOBIN=$(pwd)/bin
+	export GOPATH=$(pwd):$$(pwd)/../..:~/go
+	export PATH=$(PATH):$$(pwd)/bin
 
 gen:
 	go generate ./...
 
-migrate:
+migrations:
 	kallax migrate --input ./models --out ./data/migrations --name initial_schema
+
+migrate $(action) $(env):
+	$(foreach var,$(shell . ./bin/parse_config.sh; parse_yaml ./config/$(env).yaml 'config_'),$(eval $(var)))
+	$(eval DSN = '$(config_postgres__user):$(config_postgres__pass)@$(config_postgres__host):$(config_postgres__port)/$(config_postgres__name)?sslmode=disable' | sed 's/\"//g')
+ifeq ($(action),up)
+	kallax migrate up --dir ./data/migrations --dsn '$(shell echo $(DSN))' --all
+endif
+ifeq ($(action),down)
+	kallax migrate down --dir=./data/migrations --dsn '$(shell echo $(DSN))' -n 1
+endif
 
 release:
 	mkdir -p deploy/releases/$(NAME)-"$(VERSION)"
